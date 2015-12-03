0a0464f977052b9e94d844831b8e2e0093dbf08f
iaguas <aguas.71332@e.unavarra.es>
2015-12-03 13:35:35 +0100

    Implementación de la matriz de transformación y de la base así como de todos los métodos de transformación y conocimiento de atributos.

--- src/projection/Camera.java
+++ src/projection/Camera.java
@@ -6,6 +6,8 @@
  */
 package projection;
 
+import javax.vecmath.Matrix4f;
+import javax.vecmath.Vector4f;
 import primitives.Point3D;
 import primitives.Vector3D;
 import raytrace.RayGenerator;
@@ -16,15 +18,12 @@ import raytrace.RayGenerator;
  */
 public class Camera {
 
-    private Object lente;// Creo que es el projection de abajo
-    private Point3D viewPoint;
-    private Vector3D xbase;
-    private Vector3D ybase;
-    private Vector3D zbase;
+    private final Point3D viewPoint;
+    private final Vector3D xbase;
+    private final Vector3D ybase;
+    private final Vector3D zbase;
     private Projection lent;
-    // Parámetros:
-    // * Una matriz de la transformacion que permite pasar de coordenadas de 
-    //   camara a coordenadas de escena (inversa de la matrix de vista)
+    private final Matrix4f transformMatrix; // De camara a escena
     
     /**
      * Constructor de la cámara
@@ -33,16 +32,47 @@ public class Camera {
      * @param up Vector vertical auxiliar up.
      */
     public Camera(final Point3D viewPoint, final Vector3D look, final Vector3D up){
+        // Punto de vista
         this.viewPoint = viewPoint;
-        // TODO Calcular la base en función de look y up.
-        // TODO Luego hay que crear en algún momento la matriz de transformación (transformacion.vista.pdf)
+        
+        // Base ortonormal dextrógira 
+        this.zbase = new Vector3D(look);
+        this.xbase = (up.vecprod(zbase)).normalize();
+        this.ybase = zbase.vecprod(xbase);
+        
+        // Parámetros para la matriz de transformación
+        float s = up.escprod(look);
+        double t = Math.pow(1-s*s, -0.5f);
+        Vector3D oplook = look.oposite();
+        
+        // Creación de la matriz de transformación
+        transformMatrix = new Matrix4f();
+        transformMatrix.setColumn(1,
+            (float) t * (up.y*oplook.z - up.z*oplook.y),
+            (float) t * (up.z*oplook.x - up.x*oplook.z),
+            (float) t * (up.x*oplook.y - up.y*oplook.x),
+            0);
+        transformMatrix.setColumn(2,
+            (float) t * (up.x-s*oplook.x),
+            (float) t * (up.y-s*oplook.y),
+            (float) t * (up.z-s*oplook.z),
+            0);
+        transformMatrix.setColumn(3, new Vector4f(oplook));
+        transformMatrix.setColumn(4, new Vector4f(viewPoint));
+    }
+    
+    public Vector3D toSceneCoord(Point3D R) {
+        return new Vector3D(
+            transformMatrix.m00*R.x + transformMatrix.m01*R.y + transformMatrix.m02*R.z + transformMatrix.m03*R.w,
+            transformMatrix.m10*R.x + transformMatrix.m11*R.y + transformMatrix.m12*R.z + transformMatrix.m13*R.w,
+            transformMatrix.m20*R.x + transformMatrix.m21*R.y + transformMatrix.m22*R.z + transformMatrix.m23*R.w,
+            transformMatrix.m30*R.x + transformMatrix.m31*R.y + transformMatrix.m32*R.z + transformMatrix.m33*R.w
+        );
     }
     
     public RayGenerator getRayGenerator (final int W, final int H) {
-        return lente.getRayGenerator(this, W, H); // Esto se soluciona al acabar la lente
+        return lent.getRayGenerator(this, W, H);
     }
-    //donde lente es el nombre del atributo empleado para referenciar a la
-    // proyeccion asociada a la camara
     
     /**
      * Devuelve la projección que está siendo utilizada.
@@ -52,5 +82,19 @@ public class Camera {
         return lent;
     }
 
+    Vector3D getLook() {
+        return new Vector3D(zbase);
+    }
+
+    Point3D getPoint() {
+        return new Point3D(viewPoint);
+    }
     
+    /**
+     * Se elije la proyección con la que se debe llevar a cabo la renderización.
+     * @param proj Un objecto de tipo Projection para mostar cómo debe hacerse esta.
+     */
+    void setProjection(final Projection proj){
+        lent = proj;
+    }
 }
